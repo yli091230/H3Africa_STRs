26e71ce244884a8bdd57f200ba68f7d4aa3f71c1
Yang Li <ylil091230@gmail.com>
2022-07-08 06:05:39 +0200

    fix the error in split hipref files

--- HipSTR/script/submit_hipstr_run.sh
+++ HipSTR/script/submit_hipstr_run.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/bash
 
 
-SAMPLE=$1
+#SAMPLE=$1
 RESULT=/scratch3/users/yal084/STR_Call/HipSTR/result/H3A_Baylor
 SUCCEED_RUN=$(cat ${RESULT}/succeed_run.txt)
-REF_dir=/scratch3/users/yal084/STR_Call/HipSTR/files_for_run
-str_ref_dir=/scratch3/users/yal084/STR_Call/HipSTR/files_for_run/STR_${SAMPLE}_ref.bed
-STR_num=$(cat ${str_ref_dir} | wc -l)
+REF_DIR=/scratch3/users/yal084/STR_Call/HipSTR/files_for_run
+REF_FILE=/scratch3/users/yal084/STR_Call/HipSTR/files_for_run/hipref.bed
 SUBMISSION_LOG=$(date '+%Y-%m-%d')
 
 
-mkdir -p ${RESULT}/${SAMPLE}
+#mkdir -p ${RESULT}/${SAMPLE}
 
 start_job()
 {
@@ -22,7 +21,7 @@ start_job()
 	else
 		echo "Submitting Job ${job_name}"
 		sbatch --job-name=${job_name} \
-	       	       --output=${RESULT}/${SAMPLE}/${job_name}_slurm_%j.out \
+	       	       --output=${RESULT}/${job_name}/${job_name}_slurm_%j.out \
 	      	        /scratch3/users/yal084/STR_Call/HipSTR/script/run_hipstr.slurm $1
 		echo "${job_name} submitted" >> ${RESULT}/Jobs_${SUBMISSION_LOG}.txt
 	fi
@@ -30,10 +29,12 @@ start_job()
 
 
 
-mkdir -p ${REF_dir}/${SAMPLE}
-split -dl 26000 ${str_ref_dir} ${REF_dir}/${SAMPLE}/${SAMPLE}_ -a 2
-for splited_sample in $(ls -d ${REF_dir}/${SAMPLE}/*);
+#mkdir -p ${REF_dir}/${SAMPLE}
+split -dl 26000 ${REF_FILE} ${REF_DIR}/hipref_ -a 2
+for splited_sample in $(ls -d ${REF_DIR}/hipref_*);
 do
+	split_batch=$(basename ${splited_sample})
+	mkdir -p ${RESULT}/${split_batch}
 	#echo ${splited_sample}
 	start_job ${splited_sample}
 done
